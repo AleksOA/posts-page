d839d7faee77762336e2c9bd502766644d11f415
AleksOA <ovcharenko.alekss@gmail.com>
2023-03-03 22:07:03 +0200

    created_page_with_posts

--- js/script.js
+++ js/script.js
@@ -1,18 +1,81 @@
 
 
 
+createStartPage();
+function createStartPage() {
+    const body = document.querySelector('body');
+    body.innerHTML = `
+    <div class="container">
+        <main>
+            <div class="post__wrapper">
+                <div class="button-get-posts">
+                    <button id="buttonGetPost" >Gat post</button>
+                </div>
+            </div>
+        </main>  
+    </div>`;
+    let btnGetPost = document.getElementById('buttonGetPost');
+    btnGetPost.addEventListener('click', getPosts);
+}
+
+// Create post
+// ====================
+function createPost(data) {
+   let divPost = `
+        <div class="post">
+            <h2 class="post__title">${data.title}</h2>
+            <button class="post_btn" data-post-id="${data.id}"></button>
+        </div>   
+    `;
+    return divPost;
+}
+
+// ====================
+
+
+
+
+// Get posts
+//===========================
+
+function getPosts(){
+    outputPosts();
+}
+
+// btnGetPost.addEventListener('click', getPosts);
+//===========================
+
+// Output posts
+// ============================
+function outputPosts() {
+    let postWrapper = document.querySelector('.post__wrapper');
+    let postItems = `<div class="post__items"></div>`;
+    postWrapper.innerHTML += postItems;
+    newPage(mainSourceData(getUrlParameters()));
+}
+// ============================
+
+
+
 function updateURL(data) {
     let options_userId = data.options_userId;
-    let options_photosId = data.options_photosId;
+    let options_photosId = data.options_photosId.forBrowser;
+    let options_postId = data.options_postId.forBrowser;
     let parameterTwo = '';
-    let separator = '';
+    let separatorTwo = '';
     if(options_photosId != undefined) {
         parameterTwo = options_photosId;
-        separator = '&';
+        separatorTwo = '&';
+    }
+    let parameterThree = '';
+    let separatorThree = '';
+    if(options_postId != undefined) {
+        parameterThree = options_postId;
+        separatorThree = '&';
     }
     if (history.pushState) {
         let baseUrl = window.location.protocol + "//" + window.location.host + window.location.pathname;
-        let newUrl = `${baseUrl}?${options_userId}${separator}${parameterTwo}`;
+        let newUrl = `${baseUrl}?${options_userId}${separatorTwo}${parameterTwo}${separatorThree}${parameterThree}`;
         history.pushState(null, null, newUrl);
     }
     else {
@@ -24,43 +87,93 @@ function updateURL(data) {
 // Get URL Parameters
 //============================
 
-
 function getUrlParameters() {
     const queryString = window.location.search;
     const urlParams = new URLSearchParams(queryString);
-    let userIdFromUrl = urlParams.get('userId');
-    let idFromUrl = urlParams.get('id');
-
+    let padeIdFromBrowser = urlParams.get('userId');
+    let photoIdFromBrowser = urlParams.get('photoId');
+    let postIdFromBrowser = urlParams.get('postId');
     let queryParametersFromBrowser = {
-        'userId': userIdFromUrl,
-        'id': idFromUrl
+        'pageId': padeIdFromBrowser,
+        'photoId': photoIdFromBrowser,
+        'postId': postIdFromBrowser
     }
-    // mainSourceData(queryParametersFromBrowser);
     return queryParametersFromBrowser
 }
-
 //============================
 
 
-
 function mainSourceData(queryParametersFromBrowser) {
-    let idFromBrowser = '';
-    if(queryParametersFromBrowser.id == undefined){
-        idFromBrowser = undefined;
+    let idPhotoFromBrowser = '';
+    if(queryParametersFromBrowser.photoId == undefined){
+        idPhotoFromBrowser = undefined;
+        idPhotoFromFetch = undefined;
     }else {
-        idFromBrowser = `id=${queryParametersFromBrowser.id}`;
+        idPhotoFromBrowser = `photoId=${queryParametersFromBrowser.photoId}`;
+        idPhotoFromFetch = `id=${queryParametersFromBrowser.photoId}`;
+    }
+
+    let idPostFromBrowser = '';
+    if(queryParametersFromBrowser.postId == undefined){
+        idPostFromBrowser = undefined;
+        idPostFromFetch = undefined;
+
+    }else {
+        idPostFromBrowser = `postId=${queryParametersFromBrowser.postId}`;
+        idPostFromFetch = `id=${queryParametersFromBrowser.postId}`;
     }
 
     let mainSourceData = {
-        'options_userId': `userId=${queryParametersFromBrowser.userId}`,
-        'options_photosId': idFromBrowser
+        'options_userId': `userId=${queryParametersFromBrowser.pageId}`,
+        'options_photosId':
+            {'forBrowser': idPhotoFromBrowser,
+                'forFetch': idPhotoFromFetch
+            },
+        'options_postId':
+            {'forBrowser': idPostFromBrowser,
+                'forFetch': idPostFromFetch
+            }
     }
     updateURL(mainSourceData);
     return mainSourceData
 }
 
+function newPage(data){
+    updateURL(data);
+
+    let allPosts = new Promise((resolve, reject) => {
+        fetch('https://jsonplaceholder.typicode.com/posts')
+            .then((response) => {
+                resolve(response.json());
+            })
+    });
+
+    let page = new Promise((resolve, reject) => {
+        fetch('https://jsonplaceholder.typicode.com/posts' + `?${data.options_userId}`)
+            .then((response) => {
+                resolve(response.json());
+            })
+    });
 
 
+    Promise.all ([allPosts, page]).then(value => {
+        let allPosts = value[0];
+        let page = value[1];
+
+        // display posts on the page
+        let postItems = document.querySelector('.post__items');
+        page.forEach(post => {
+            postItems.innerHTML += createPost(post);
+        });
+
+    });
+
+}
+
+
+
+
+// ================================================================================================================
 
 function newPostsPage(data) {
     updateURL(data);
@@ -73,29 +186,36 @@ function newPostsPage(data) {
         .then((response) => response.json())
         .then((json) => console.log(json));
 
-    console.log(data.options_photosId);
-    if(data.options_photosId != undefined) {
-        fetch('https://jsonplaceholder.typicode.com/albums/1/photos' + `?${data.options_photosId}`)
+
+    if(data.options_photosId.forFetch != undefined) {
+        fetch('https://jsonplaceholder.typicode.com/albums/1/photos' + `?${data.options_photosId.forFetch}`)
             .then((response) => response.json())
             .then((json) => console.log(json));
     }
 
-
+    if(data.options_postId.forFetch != undefined) {
+        fetch('https://jsonplaceholder.typicode.com/posts' + `?${data.options_postId.forFetch}`)
+            .then((response) => response.json())
+            .then((json) => console.log(json));
+    }
 }
 
 
+// output data from URL same had enter browser````
 
-// output data from URL same had enter browser
 // ================
-newPostsPage(mainSourceData(getUrlParameters()));
+// newPostsPage(mainSourceData(getUrlParameters()));
 //=================
 
 
 
 let variableData = {
-    'userId': 5,
-    // 'id': 3
+    'pageId': 7,
+    'photoId': 5,
+    'postId': 5
+
 }
-newPostsPage(mainSourceData(variableData));
+
+// newPostsPage(mainSourceData(variableData));
 
 
